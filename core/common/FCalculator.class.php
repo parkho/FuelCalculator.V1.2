ee7d7b0ad8f5a274a01403d9b94aa38c712f930a
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-05-20 16:09:34 +0430

    Code Update
    Some codes where missing from the common file

--- core/common/FCalculator.class.php
+++ core/common/FCalculator.class.php
@@ -44,6 +44,49 @@ class FCalculator extends CodonData
 			return DB::get_row($sql);
 		}
 	
+	public static function calculatefuel($aircraft, $distance)
+		{
+			$sql = "SELECT * FROM fuel_param WHERE aircraft='$aircraft'";
+			$fuel = DB::get_row($sql);
+			$flow = $fuel->flow;
+			$fuelhr = $fuel->hour;
+			$speed = $fuel->speed;
+			$fueldistance = $distance / 100 ;
+			$fuelreserve = $fuelhr * 3/4;
+			$fueltaxi = 200;
+			$fuelnm = $flow * $fueldistance;
+			
+			$total = $fuelnm + $fueltaxi + $fuelreserve;
+			
+			
+			return $total;
+		}
+	
+	public static function pilot_pay($pilotid)
+		{
+			$ft = "SELECT flighttime FROM phpvms_pireps WHERE pilotid = '$pilotid' ORDER BY submitdate DESC";
+			$flttme = DB::get_row($ft);
+			$hr = intval($flttme->flighttime);
+			$mn = ($flttme->flighttime - $hr) * 100;
+			$min = ($hr * 60) + $mn;
+						
+			$fr = "SELECT pilotpay FROM phpvms_pireps WHERE pilotid = '$pilotid' ORDER BY submitdate DESC";
+			$payrate = DB::get_row($fr);
+			$pay = $payrate->pilotpay / 60;
+			
+			$pp = "SELECT totalpay FROM phpvms_pilots WHERE pilotid = '$pilotid'";
+			$ppay = DB::get_row($pp);
+			$pipay = $payrate->totalpay;			
+			$ftupdt = $min * $pay;
+			$ttalpay = $ftupdt + $pipay;
+			$totalpay = ROUND($ttalpay, 2);
+			
+			$updt = "UPDATE phpvms_pilots SET totalpay = '$totalpay' + totalpay WHERE pilotid = '$pilotid'";
+			
+			DB::query($updt);		
+	
+		}
+	
 	
 }
 ?>
