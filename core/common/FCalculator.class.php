a2253c9588afb720e67668b2763e03aca672180d
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-05-23 10:24:25 +0430

    Updated functions
    Updated functions

--- core/common/FCalculator.class.php
+++ core/common/FCalculator.class.php
@@ -1,44 +1,44 @@
 <?php
 class FCalculator extends CodonData
 {
-	public function findaircraft() 
+	public static function findaircraft() 
 		{
 			$sql = "SELECT DISTINCT name FROM".TABLE_PREFIX."aircraft";
 			return DB::get_results($sql);
 		}
 	
-	public function param($aircraft, $ff, $fh, $fr, $fs, $fa)
+	public static function param($aircraft, $ff, $fh, $fr, $fs, $fa)
 		{
 			$sql = "INSERT INTO fuel_param (aircraft, flow, hour, frange, speed, alt, date) 
 								  VALUES ('$aircraft', '$ff', '$fh', '$fr', '$fs', '$fa', NOW())";
 			DB::query($sql);
 		}
 	
-	public function editparam($ff, $fh, $fr, $fs, $fa, $id)
+	public static function editparam($ff, $fh, $fr, $fs, $fa, $id)
 		{
 			$sql = "UPDATE fuel_param SET flow='$ff', hour='$fh', frange='$fr', speed='$fs', alt='$fa', date=NOW() WHERE id='$id'";
 			DB::query($sql);
 		}
 	
-	public function getparam()
+	public static function getparam()
 		{
 			$sql = "SELECT * FROM fuel_param";
 			return DB::get_results($sql);
 		}
 	
-	public function getparamaircraft($aircraft)
+	public static function getparamaircraft($aircraft)
 		{
 			$sql = "SELECT * FROM fuel_param WHERE aircraft='$aircraft'";
 			return DB::get_row($sql);
 		}
 	
-	public function remove($id)
+	public static function remove($id)
 		{
 			$sql = "DELETE FROM fuel_param WHERE id='$id'";
 			DB::query($sql);
 		}
 	
-	public function getparambyid($id)
+	public static function getparambyid($id)
 		{
 			$sql = "SELECT * FROM fuel_param WHERE id='$id'";
 			return DB::get_row($sql);
